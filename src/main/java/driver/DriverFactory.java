842c39f5f49ec22a6e399ac77f40c5f5700e383d
Mariya Krivosheeva <99337784+MKrivosheeva@users.noreply.github.com>
2022-11-11 21:49:10 +0300

    Update DriverFactory.java

--- src/main/java/driver/DriverFactory.java
+++ src/main/java/driver/DriverFactory.java
@@ -6,8 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.Locale;
-
 public class DriverFactory implements IDriverFactory {
 
    private String browserType = System.getProperty("browser").toLowerCase();
